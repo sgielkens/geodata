eb463c5218352452c1336221bbe004c521eb18bd
Serge Gielkens <serge@globe10.leusnet.teld>
2023-11-27 20:13:30 +0100

    Test op input dir Remove trailing blanks

--- tools/zip_photos_trackwise.py
+++ tools/zip_photos_trackwise.py
@@ -51,6 +51,13 @@ if args.input == None:
 else:
     input_path = args.input
 
+input_dir = pathlib.Path(input_path)
+
+if not input_dir.is_dir():
+    print("Input directory does not exist: " + input_path)
+    sys.exit(1)
+
+
 
 if args.output == None:
     output_path = dir_path + '/' + zip_dir
@@ -63,11 +70,9 @@ if not output_dir.is_dir():
     output_dir.mkdir()
 else:
     if any(os.scandir(output_path)):
-        print("The output directory is not empty: " + output_path + ". Quitting." )
+        print("Output directory is not empty: " + output_path + ". Quitting." )
         sys.exit(1)
 
-
-sys.exit(42)
 
 #with zipfile.ZipFile(dir_path + "/zip/py_test.zip", mode="r") as archive:
 #    archive.printdir()
@@ -83,12 +88,12 @@ directory = pathlib.Path(input_path)
 
 #tellers = []
 tellers = {}
-            
+
 for file_path in directory.iterdir():
     woord = file_path.name
 #    print(woord)
     lettergreep = woord.split('_')
-    
+
     track_naam = lettergreep[3]
     track_nummer = int(track_naam.lstrip("Track"))
 #    print(track_nummer)
@@ -98,32 +103,32 @@ for file_path in directory.iterdir():
     if camera.endswith(".txt"):
         camera = camera.removesuffix(".txt")
         camera = camera + "_int_ort"
-        
+
     if camera.endswith(".csv"):
         camera = camera.removesuffix(".csv")
         camera = camera + "_ext_ort"
-      
-      
+
+
     if not track_naam in tellers:
         tellers[track_naam] = {}
         tellers[track_naam]["totaal"] = 0
-        
+
     tellers[track_naam]["totaal"] += 1
-    
+
     if not camera in tellers[track_naam]:
         tellers[track_naam][camera] = 0
-        
+
     tellers[track_naam][camera] += 1
-    
-    
-            
-    zip_bestand = dir_path + "/zip/fotos_" + track_naam + ".zip"
-    
+
+
+
+    zip_bestand = output_path + "/fotos_" + track_naam + ".zip"
+
     if os.path.exists(zip_bestand):
         zip_modus = 'a'
     else:
         zip_modus = 'w'
-        
+
     with zipfile.ZipFile(zip_bestand, mode = zip_modus) as archive:
         archive.write(file_path, arcname=file_path.name)
 
@@ -135,29 +140,29 @@ for track, aantallen in tellers.items():
         if not camera.endswith("_ort") and not camera == "totaal":
             nr_fotos = aantal
             break
-    
-#print(nr_fotos)    
 
-  
+#print(nr_fotos)
+
+
 for track, aantallen in tellers.items():
     print(track + ': ' + str( aantallen["totaal"] ) + ' bestanden in totaal')
-    
+
     stop = False
-    
+
     aantal_int_ort = 0
     aantal_ext_ort = 0
-    
+
     for camera, aantal in aantallen.items():
         if camera == "totaal":
             continue
-        
+
         if camera.endswith("_ort"):
             if camera.endswith("_int_ort"):
                 aantal_int_ort += 1
                 if aantal != 1:
                     print(camera + ": expected 1 internal orientation file but found " + aantal)
                     stop = True
-                
+
             else:
                 aantal_ext_ort += 1
                 if aantal != 1:
@@ -167,19 +172,19 @@ for track, aantallen in tellers.items():
         elif aantal != nr_fotos:
             print( "Aantal foto's " + str(aantal) + " van " + camera + " verschilt van " + str(nr_fotos) )
             stop = True
-        
+
     if aantal_int_ort != 9:
         print( track + ": expected 9 internal orientation txt files but found " + str(aantal_int_ort) )
         stop = True
-            
+
     if aantal_ext_ort != 9:
         print( track + ": expected 9 external orientation csv files but found " + str(aantal_ext_ort) )
         stop = True
-            
+
     print("")
-    
+
     if stop == True:
         print(track + ": unexpected number of files. See the logging above")
         break
-    
-    
+
+
