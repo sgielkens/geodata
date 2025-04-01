31361b6a78cc903cba25fb588f2c24d1e64f6842
Serge Gielkens <serge.gielkens@landmeten.nl>
2025-04-01 11:34:00 +0200

    Aanpassing scripts voor ondergronden maart 2025

---
+++ kadaster/bat/1-BAG-inladen.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bag_light.gpkg -overwrite -progress
--- kadaster/bat/1-BGT-en-DKK-inladen.bat
+++ kadaster/bat/1-BGT-en-DKK-inladen.bat
@@ -1,12 +1,12 @@
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_scheiding.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_kunstwerkdeel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_waterdeel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_wegdeel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_ondersteunendwaterdeel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_openbareruimtelabel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_ondersteunendwegdeel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" dkk_perceel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" dkk_openbareruimtelabel.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" dkk_kadastralegrens.gml -overwrite -progress
-"C:\Program Files\QGIS 3.22.7\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" dkk_pand.gml -overwrite -progress
-pause
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_scheiding.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_kunstwerkdeel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_waterdeel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_wegdeel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_ondersteunendwaterdeel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_openbareruimtelabel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" bgt_ondersteunendwegdeel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" kadastralekaart_perceel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" kadastralekaart_openbareruimtelabel.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" kadastralekaart_kadastralegrens.gml -overwrite -progress
+"C:\Program Files\QGIS 3.40.4\bin\ogr2ogr" -f "PostgreSQL" PG:"host=localhost port=5432 dbname=kadastraal user=postgres password=postgres" kadastralekaart_pand.gml -overwrite -progress
+
