ac265128787cd4079fe463a401e30127ca3b33e4
Serge Gielkens <serge.gielkens@landmeten.nl>
2025-04-07 13:37:43 +0200

    Melding als (u)mount niet lukt

--- tools/mount_ext4.ps1
+++ tools/mount_ext4.ps1
@@ -1,7 +1,9 @@
 $disk = (GET-CimInstance -ClassName Win32_DiskDrive -Filter "Caption like '%SCSI%'" | select DeviceID -ExpandProperty DeviceID)
 
-if ($disk){
+if (-not $disk){
 	wsl --mount $disk --partition 1
 	explorer \\wsl.localhost\Ubuntu\mnt\wsl
 }
-
+else {
+	echo "Could not determine disk device"
+}
--- tools/umount_ext4.ps1
+++ tools/umount_ext4.ps1
@@ -1,5 +1,8 @@
 $disk = (GET-CimInstance -ClassName Win32_DiskDrive -Filter "Caption like '%SCSI%'" | select DeviceID -ExpandProperty DeviceID)
 
-if ($disk){
+if (-not $disk){
 	wsl --unmount $disk
+}
+else {
+	echo "Could not determine disk device"
 }
