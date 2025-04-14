6984181ecdcc638dc2cce8682d70c459892b9075
Serge Gielkens <serge.gielkens@landmeten.nl>
2025-04-14 15:08:10 +0200

    Extra test op meerdere disks

--- tools/umount_ext4.ps1
+++ tools/umount_ext4.ps1
@@ -1,6 +1,15 @@
 #Requires -RunAsAdministrator
 
-$disk = (GET-CimInstance -ClassName Win32_DiskDrive -Filter "Caption like '%SCSI%'" | select DeviceID -ExpandProperty DeviceID)
+$disk = (GET-CimInstance -ClassName Win32_DiskDrive -Filter "Caption like '%SCSI%' and not Caption like '%Portable%'" | select DeviceID -ExpandProperty DeviceID)
+
+$number = (echo $disk | Measure-Object).Count
+
+if ($number -ne 1) {
+	echo "More than 1 candidate disk detected"
+	echo $disk
+	echo "Remove external disks other than the disk reader"
+	exit
+}
 
 if (-not $disk){
 	echo "Could not determine disk device"
